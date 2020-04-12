424ac1b7dd98cdd8cf7f9ded9e39982fa4916625
Nick Pan <panick@panick-macbookpro3.roam.corp.google.com>
2020-04-12 18:46:45 -0400

    Adjust filter word to 'coronavirus'

--- ps5.py
+++ ps5.py
@@ -33,7 +33,7 @@ def process(url):
         guid = entry.guid
         title = translate_html(entry.title)
         link = entry.link
-        description = translate_html(entry.description)
+        #description = translate_html(entry.description)
         pubdate = translate_html(entry.published)
 
         try:
@@ -44,7 +44,7 @@ def process(url):
         except ValueError:
             pubdate = datetime.strptime(pubdate, "%a, %d %b %Y %H:%M:%S %z")
 
-        newsStory = NewsStory(guid, title, description, link, pubdate)
+        newsStory = NewsStory(guid, title, '', link, pubdate)
         ret.append(newsStory)
     return ret
 
@@ -249,10 +249,14 @@ def filter_stories(stories, triggerlist):
 
     Returns: a list of only the stories for which a trigger in triggerlist fires.
     """
-    # TODO: Problem 10
-    # This is a placeholder
-    # (we're just returning all the stories, with no filtering)
-    return stories
+    filtered_stories = []
+    for story in stories:
+        for trigger in triggerlist:
+            if trigger.evaluate(story):
+                filtered_stories.append(story)
+                continue
+
+    return filtered_stories
 
 
 
@@ -290,7 +294,7 @@ def main_thread(master):
     # A sample trigger list - you might need to change the phrases to correspond
     # to what is currently in the news
     try:
-        t1 = TitleTrigger("election")
+        t1 = TitleTrigger("corona")
         t2 = DescriptionTrigger("Trump")
         t3 = DescriptionTrigger("Clinton")
         t4 = AndTrigger(t2, t3)
